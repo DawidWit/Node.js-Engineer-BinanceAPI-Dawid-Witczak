a447cf8f3150f1b6d8dc1f16571fbc0a1640c6d6
DawidWit <135219147+DawidWit@users.noreply.github.com>
2025-06-10 10:10:57 +0200

    first commit

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//CONNECTS MONGODB DATABASE TO APPLICATION 
+connectDB = async () => {
+    try {
+        const dbConn = mongoose.connect(process.env.MONGO_CONN_STR);
+        console.log("MONGODB CONNECTED");
+        return dbConn;
+    } catch (error) {
+        console.error('MONGODB CONNECTION ERROR: ', error.message);
+        process.exit(1);
+    }
+}
