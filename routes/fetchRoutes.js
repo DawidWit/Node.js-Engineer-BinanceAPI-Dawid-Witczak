29be8222bddb2ca6f45cd1a296469b0dc8052c5c
DawidWit <135219147+DawidWit@users.noreply.github.com>
2025-06-10 11:33:18 +0200

    commit

--- routes/fetchRoutes.js
+++ routes/fetchRoutes.js
@@ -6,28 +6,27 @@ require('dotenv').config();
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-
+    console.log(req.query);
     if (!process.env.BINANCE_BASE_STR) {
         console.error('NO BINANCE BASE PROVIDED');
         return res.status(500).send({ msg: 'NO BINANCE BASE PROVIDED' });
     }
-    //!req.params.startDate || !req.params.endDate ||
-    /* if (!req.params.symbol) {
-         return res.status(400).send({ msg: 'NO REQUIRED PARAMS' });
-     }*/
-
+    if (!req.query.startDate || !req.query.endDate || !req.query.symbol) {
+        return res.status(400).send({ msg: 'NO REQUIRED PARAMS' });
+    }
+    console.log(new Date("2025-05-15").getTime());
+    console.log(new Date("2015-05-29").getTime());
     try {
         const currentPrice = await fetchCurrentSymbol('BTCUSDT');
-        console.log(currentPrice);
         const result = await axios.get(`${process.env.BINANCE_BASE_STR}/klines`, {
             params: {
                 symbol: 'BTCUSDT',
                 interval: '3d',
-                //startDate: new Date("2025-03-25").getTime(),
-                //endDate: new Date("2015-03-29").getTime()
+                startTime: new Date("2025-05-15").getTime(),
+                endTime: new Date("2015-05-29").getTime()
             }
         });
-        console.log(result);
+        console.log(result.data);
     } catch (error) {
         console.error('ERROR FETCHING HISTORICAL SYMBOL DATA: ', error.res?.data?.msg || error.message);
         return res.status(500).send({ msg: `ERROR FETCHING HISTORICAL SYMBOL DATA:  ${error.res?.data?.msg || error.message}` });
